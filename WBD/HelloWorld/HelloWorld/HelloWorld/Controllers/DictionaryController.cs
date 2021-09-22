f5ea04df20705dc27f4b035470d56a17d0e56a92
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2021-09-22 11:33:46 +0700

    Dictionary

---
+++ WBD/HelloWorld/HelloWorld/HelloWorld/Controllers/DictionaryController.cs
@@ -0,0 +1,45 @@
+﻿using HelloWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        public Dictionary<string, string> Dictionaries = 
+                new Dictionary<string, string>() {
+                    {"Xin chào", "Hello" },
+                    { "Quả táo", "Apple" },
+                    { "Máy tính xách tay", "Laptop" },
+                    { "Quả cà chua", "Toamto" }
+                };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Translate());
+        }
+
+        [HttpPost]
+        public IActionResult Translate(Translate model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Result = "";
+                foreach(KeyValuePair<string, string> item in Dictionaries)
+                {
+                    if(item.Key.ToLower() == model.Keyword.ToLower())
+                    {
+                        model.Result = item.Value;
+                        break;
+                    }
+                }
+                return View("~/Views/Dictionary/Index.cshtml", model);
+            }
+            return View("~/Views/Dictionary/Index.cshtml");
+        }
+    }
+}
---
+++ WBD/HelloWorld/HelloWorld/HelloWorld/Models/Translate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Models
+{
+    public class Translate
+    {
+        [Required]
+        public string Keyword { get; set; }
+        public string Result { get; set; }
+    }
+}
