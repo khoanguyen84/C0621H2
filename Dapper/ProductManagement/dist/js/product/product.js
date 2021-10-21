de59ffb4a5ce2748a824f37b970447707ccb9e3e
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2021-10-21 11:08:04 +0700

    build create edit product

--- Dapper/ProductManagement/dist/js/product/product.js
+++ Dapper/ProductManagement/dist/js/product/product.js
@@ -1,13 +1,16 @@
 var product = {};
 
-product.showData = function(){
+product.showData = function () {
     $.ajax({
-        url : "https://616e3eb5a83a850017caa8a6.mockapi.io/products",
-        method : "GET",
-        contentType : "JSON",
-        success : function(data){
-            console.log(data);
-            $.each(data, function(index, item){
+        url: "https://616e3eb5a83a850017caa8a6.mockapi.io/products",
+        method: "GET",
+        datatype: "json",
+        success: function (data) {
+            data.sort(function (item1, item2) {
+                return item2.id - item1.id;
+            });
+            $('#tbProduct').empty();
+            $.each(data, function (index, item) {
                 $('#tbProduct').append(`
                     <tr>
                         <td>${item.id}</td>
@@ -24,7 +27,7 @@ product.showData = function(){
                             </span>
                         </td>
                         <td>
-                            <a href="javascript:;" class="btn btn-primary btn-sm">
+                            <a href="javascript:;" class="btn btn-primary btn-sm" onclick='product.getProduct(${item.id})'>
                                 <i class="fa fa-edit"></i>
                             </a>
                             <a href="javascript:;" class="btn ${item.Status ? 'btn-warning' : 'bg-secondary'}  btn-sm">
@@ -41,6 +44,107 @@ product.showData = function(){
     });
 }
 
-$(document).ready(function(){
+
+product.openModal = function () {
+    $('#addEditProduct').modal('show');
+    product.reset();
+}
+
+product.save = function () {
+    if ($('#addEditForm').valid()) {
+        if(Number($('#productId').val()) == 0){
+            let createObj = {
+                ProductName: $('#productName').val(),
+                Manufactory: $('#manufactory').val(),
+                Price: $('#price').val(),
+                Quantity: Number($('#quantity').val()),
+                Photo: "http://placeimg.com/640/480",
+                Status: $('#status').is(':checked'),
+            };
+            $.ajax({
+                url: "https://616e3eb5a83a850017caa8a6.mockapi.io/products",
+                method: "POST",
+                contentType: "application/json",
+                datatype: "json",
+                data: JSON.stringify(createObj),
+                success: function (result) {
+                    if (result) {
+                        $('#addEditProduct').modal('hide');
+                        $.notify("Product has been created successfully!", "success");
+                        product.showData();
+                    }
+                    else {
+                        $.notify("Something went wrong, please try again!", "error");
+                    }
+    
+                }
+            });
+        }
+        else{
+            let updateObj = {
+                id: Number($('#productId').val()),
+                ProductName: $('#productName').val(),
+                Manufactory: $('#manufactory').val(),
+                Price: $('#price').val(),
+                Quantity: Number($('#quantity').val()),
+                Photo: "http://placeimg.com/640/480",
+                Status: $('#status').is(':checked'),
+            };
+            $.ajax({
+                url: `https://616e3eb5a83a850017caa8a6.mockapi.io/products/${updateObj.id}`,
+                method: "PUT",
+                contentType: "application/json",
+                datatype: "json",
+                data: JSON.stringify(updateObj),
+                success: function (result) {
+                    if (result) {
+                        $('#addEditProduct').modal('hide');
+                        $.notify("Product has been updated successfully!", "success");
+                        product.showData();
+                    }
+                    else {
+                        $.notify("Something went wrong, please try again!", "error");
+                    }
+    
+                }
+            });
+        }
+
+        
+    }
+}
+
+product.reset = function () {
+    $("#addEditForm").validate().resetForm();
+    $('#productName').val("");
+    $('#manufactory').val("");
+    $('#price').val("");
+    $('#quantity').val("");
+    $('#photo').val("http://placeimg.com/640/480");
+    $('#status').prop("checked", false);
+    $('#productId').val(0);
+    $('#addEditProduct').find(".modal-title").text("Create Product");
+}
+
+product.getProduct = function (id) {
+    $.ajax({
+        url: `https://616e3eb5a83a850017caa8a6.mockapi.io/products/${id}`,
+        method: "GET",
+        datatype: "json",
+        success: function (product) {
+            $('#productName').val(product.ProductName);
+            $('#manufactory').val(product.Manufactory);
+            $('#price').val(product.Price);
+            $('#quantity').val(product.Quantity);
+            $('#photo').val(product.Photo);
+            $('#status').prop("checked", product.Status);
+            $('#productId').val(product.id);
+            $('#addEditProduct').find(".modal-title").text("Update Product");
+            $('#addEditProduct').modal('show');
+        }
+    });
+}
+
+$(document).ready(function () {
     product.showData();
 });
